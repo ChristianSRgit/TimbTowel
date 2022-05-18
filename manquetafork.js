0ca96d14582e1782b678767ef535a633e8c70d15
Christian.s.r.Home <christian.s.ramundo@gmail.com>
2022-05-18 20:47:26 -0300

    Se agregaron funciones Agregar/sacar visibilidad de niveles proximos

--- manquetafork.js
+++ manquetafork.js
@@ -2,6 +2,15 @@
 // selecciono los botones con queryselectors, algunos por clase otros por id
 let Start = document.querySelector(".randomGen");
 
+    Start.addEventListener("click",randomGen1); 
+//filas
+const fila1 = document.querySelector("#fila1");
+const fila2 = document.querySelector("#fila2");
+const fila3 = document.querySelector("#fila3");
+const fila4 = document.querySelector("#fila4");
+const fila5 = document.querySelector("#fila5");
+
+//botones
 let r1_1 = document.querySelector("#r1_1");
 let r1_2 = document.querySelector("#r1_2");
 let r1_3 = document.querySelector("#r1_3");
@@ -22,7 +31,8 @@ let r5_1 = document.querySelector("#r5_1");
 let r5_2 = document.querySelector("#r5_2");
 let r5_3 = document.querySelector("#r5_3");
 
-
+//estaria bueno hacer una matriz fija para que aunque aparezcan/desaparezcan las filas no modifique
+//la altura ya que queda horrendo
 
 //ganadores de cada nivel
 function DeclareWiners(){
@@ -50,8 +60,19 @@ const r5_3Wins =  false;
 
 
  iniciarApp();
-
+ 
 function iniciarApp(){
+    Start = false; 
+    //alert("Presiona start para iniciar TimbTowel");
+//aca deberia bloquear todos los botones menos la primera fila
+fila2.classList.add("notVisible");
+fila3.classList.add("notVisible");
+fila4.classList.add("notVisible");
+fila5.classList.add("notVisible");
+
+
+    
+    
     
     
 };
@@ -60,9 +81,9 @@ function iniciarApp(){
 
 function Loss(){
     alert("Loss");
-}
+    allrowsNotVisible();
 
-Start.addEventListener("click",randomGen1); 
+}
 
 
 
@@ -77,6 +98,7 @@ let WinnerRound5 = 0;
 //Generador de primeros 3 random resultado de un num random x pi x 20 y redondeado
 function randomGen1(){
     if(Start = true){
+        alert("elige uno de los 3, solo hay 1 correcto,si elegis bien, pasas al prox nivel")
         
         r1_1 = Math.random() * 20 * Math.PI ;
         r1_1 = Math.round(r1_1);
@@ -93,9 +115,14 @@ function randomGen1(){
      const WinnerRowA = Math.max(...RowA3Numbers);
 
      WinnerRound1 = WinnerRowA;
+
     }
 };
 
+function randomGen1Visible(){
+    //fila1.classList.remove("notVisible");
+
+};
 function randomGen2(){
         
             
@@ -115,6 +142,10 @@ function randomGen2(){
     
          WinnerRound2 = WinnerRowB;
         
+};
+
+function randomGen2Visible(){
+    fila2.classList.remove("notVisible");
 };
 
 function randomGen3(){
@@ -137,6 +168,10 @@ function randomGen3(){
             
 };
 
+function randomGen3Visible(){
+    fila3.classList.remove("notVisible");
+};
+
 function randomGen4(){
                     
                     r4_1 = Math.random() * 20 * Math.PI ;
@@ -155,6 +190,10 @@ function randomGen4(){
             
                  WinnerRound4 = WinnerRowD;
                 
+};
+
+function randomGen4Visible(){
+    fila4.classList.remove("notVisible");
 };
 
 function randomGen5(){
@@ -177,198 +216,224 @@ function randomGen5(){
                     
 };
 
+function randomGen4Visible(){
+    fila5.classList.remove("notVisible");
+};
 
+function allrowsNotVisible(){
+fila2.classList.add("notVisible");
+fila3.classList.add("notVisible");
+fila4.classList.add("notVisible");
+fila5.classList.add("notVisible");
+
+};
+
+
+function resetAll(){
+    
+    
+};
 
     Runlvl1();
-
-    function Runlvl1(){
-     //espero el click en el primer nivel
-    r1_1.addEventListener("click",EligionR1_1);
-    r1_2.addEventListener("click",EligionR1_2);
-    r1_3.addEventListener("click",EligionR1_3);
+    function Runlvl1(){    //espero el click en el primer nivel
+                r1_1.addEventListener("click",EligionR1_1);
+                r1_2.addEventListener("click",EligionR1_2);
+                r1_3.addEventListener("click",EligionR1_3);
  
 
-function EligionR1_1(){
-    if(r1_1 >= WinnerRound1){
-         r1_1Wins = true;
-         randomGen2();
+                function EligionR1_1(){
+                     if(r1_1 >= WinnerRound1){
+                        r1_1Wins = true;
+                        randomGen2();
+                        randomGen2Visible();
          
-} else{
-         Loss();
-}
-}
+                   } else{
+                        Loss();
+                        allrowsNotVisible();
+                       
+                   }
+                }
 
-function EligionR1_2(){
-     if(r1_2 >= WinnerRound1){
-         r1_2Wins = true;
-         randomGen2();
-} else{
-         Loss();
-}
-}
+               function EligionR1_2(){
+                       if(r1_2 >= WinnerRound1){
+                          r1_2Wins = true;
+                          randomGen2();
+                          randomGen2Visible();
+                     } else{
+                          Loss();
+                     }
+               }
 
-function EligionR1_3(){
-    if(r1_3 >= WinnerRound1){
-         r1_3Wins = true;
-         randomGen2();
-} else{
-        Loss();
-}
-}
+               function EligionR1_3(){
+                     if(r1_3 >= WinnerRound1){
+                        r1_3Wins = true;    
+                        randomGen2();
+                        randomGen2Visible();
+                     } else{
+                          Loss();
+                     }
+               }
     };
+
     //arrnca aca el segundo nivel
     Runlvl2();
 
     function Runlvl2(){
-        //espero el click en el primer nivel
+        //espero el click en el segundo nivel
        r2_1.addEventListener("click",EligionR2_1);
        r2_2.addEventListener("click",EligionR2_2);
        r2_3.addEventListener("click",EligionR2_3);
     
    
-   function EligionR2_1(){
-       if(r2_1 >= WinnerRound2){
-            r2_1Wins = true;
-            randomGen3();
-            
-   } else{
-            Loss();
-   }
-   }
+         function EligionR2_1(){
+               if(r2_1 >= WinnerRound2){
+                  r2_1Wins = true;
+                  randomGen3();
+                  randomGen3Visible();
+                  } else{
+                       Loss();
+                  }
+        }
    
-   function EligionR2_2(){
-        if(r2_2 >= WinnerRound2){
-            r2_2Wins = true;
-            randomGen3();
-   } else{
-            Loss();
-   }
-   }
+        function EligionR2_2(){
+                if(r2_2 >= WinnerRound2){
+                   r2_2Wins = true;
+                   randomGen3();
+                   randomGen3Visible();
+                 } else{
+                      Loss();
+                 }
+        }
    
-   function EligionR2_3(){
-       if(r2_3 >= WinnerRound2){
-            r1_3Wins = true;
-            randomGen3();
-   } else{
-           Loss();
-   }
-   }
+        function EligionR2_3(){
+              if(r2_3 >= WinnerRound2){
+                 r1_3Wins = true;
+                 randomGen3();
+                 randomGen3Visible();
+                } else{
+                     Loss();
+                }
+        }
     };
-    //arrnca aca el tercer nivel
+    //arranca aca el tercer nivel
     Runlvl3();
 
     function Runlvl3(){
-        //espero el click en el primer nivel
+        //espero el click en el tercer nivel
        r3_1.addEventListener("click",EligionR3_1);
        r3_2.addEventListener("click",EligionR3_2);
        r3_3.addEventListener("click",EligionR3_3);
     
    
-   function EligionR3_1(){
-       if(r3_1 >= WinnerRound3){
-            r3_1Wins = true;
-            randomGen4();
-            
-   } else{
-            Loss();
-   }
-   }
+         function EligionR3_1(){
+             if(r3_1 >= WinnerRound3){
+                r3_1Wins = true;
+                randomGen4();   
+                randomGen4Visible();     
+           } else{
+                Loss();
+           }
+         }
    
-   function EligionR3_2(){
-        if(r3_2 >= WinnerRound3){
-            r3_2Wins = true;
-            randomGen4();
-   } else{
-            Loss();
-   }
-   }
+         function EligionR3_2(){
+             if(r3_2 >= WinnerRound3){
+                r3_2Wins = true;
+                randomGen4();
+                randomGen4Visible();  
+           } else{
+                Loss();
+           }
+         }
    
-   function EligionR3_3(){
-       if(r3_3 >= WinnerRound3){
-            r1_3Wins = true;
-            randomGen4();
-   } else{
-           Loss();
-   }
-   }
+         function EligionR3_3(){
+             if(r3_3 >= WinnerRound3){
+                r1_3Wins = true;
+                randomGen4();
+                randomGen4Visible();  
+           } else{
+                Loss();
+           }
+         }
     };
 
-    //arrnca aca el cuarto nivel
+    //arranca aca el cuarto nivel
     Runlvl4();
 
-    function Runlvl4(){
-        //espero el click en el primer nivel
-       r4_1.addEventListener("click",EligionR4_1);
-       r4_2.addEventListener("click",EligionR4_2);
-       r4_3.addEventListener("click",EligionR4_3);
+         function Runlvl4(){
+        //espero el click en el cuarto nivel
+           r4_1.addEventListener("click",EligionR4_1);
+           r4_2.addEventListener("click",EligionR4_2);
+           r4_3.addEventListener("click",EligionR4_3);
     
    
-   function EligionR4_1(){
-       if(r4_1 >= WinnerRound4){
-            r4_1Wins = true;
-            randomGen5();
-            
-   } else{
-            Loss();
-   }
-   }
+            function EligionR4_1(){
+                 if(r4_1 >= WinnerRound4){
+                    r4_1Wins = true;
+                    randomGen5(); 
+                    randomGen5Visible();  
+                           
+               } else{
+                    Loss();
+               }
+            }
    
-   function EligionR4_2(){
-        if(r4_2 >= WinnerRound4){
-            r4_2Wins = true;
-            randomGen5();
-   } else{
-            Loss();
-   }
-   }
+             function EligionR4_2(){
+                  if(r4_2 >= WinnerRound4){
+                     r4_2Wins = true;
+                     randomGen5();
+                     randomGen5Visible();  
+                } else{
+                     Loss();
+                }
+             }
    
-   function EligionR4_3(){
-       if(r4_3 >= WinnerRound4){
-            r4_3Wins = true;
-            randomGen5();
-   } else{
-           Loss();
-   }
-   }
+             function EligionR4_3(){
+                  if(r4_3 >= WinnerRound4){
+                     r4_3Wins = true;
+                     randomGen5();
+                     randomGen5Visible();  
+                } else{
+                     Loss();
+                }
+            }
     };
        //aca empieza el 5/ultimo nivel :D
     Runlvl5();
 
-    function Runlvl5(){
-        //espero el click en el primer nivel
-       r5_1.addEventListener("click",EligionR5_1);
-       r5_2.addEventListener("click",EligionR5_2);
-       r5_3.addEventListener("click",EligionR5_3);
+            function Runlvl5(){
+              //espero el click en el ultimo nivel
+                 r5_1.addEventListener("click",EligionR5_1);
+                 r5_2.addEventListener("click",EligionR5_2);
+                 r5_3.addEventListener("click",EligionR5_3);
     
    
-   function EligionR5_1(){
-       if(r5_1 >= WinnerRound5){
-            r5_1Wins = true;
-            FinalWinWin();
-            
-   } else{
-            Loss();
-   }
-   }
+                function EligionR5_1(){
+                     if(r5_1 >= WinnerRound5){
+                        r5_1Wins = true;
+                        FinalWinWin();       
+                   } else{
+                        Loss();
+                   }
+                }
    
-   function EligionR5_2(){
-        if(r5_2 >= WinnerRound5){
-            r5_2Wins = true;
-            FinalWinWin();
-   } else{
-            Loss();
-   }
-   }
+                function EligionR5_2(){
+                     if(r5_2 >= WinnerRound5){
+                        r5_2Wins = true;
+                        FinalWinWin();
+                   } else{
+                        Loss();
+                   }
+                }
    
-   function EligionR5_3(){
-       if(r5_3 >= WinnerRound5){
-            r5_3Wins = true;
-            FinalWinWin();
-   } else{
-           Loss();
-   }
-   }
-    };
+                function EligionR5_3(){
+                     if(r5_3 >= WinnerRound5){
+                        r5_3Wins = true;
+                        FinalWinWin();
+                   } else{
+                        Loss();
+                   }
+                }
+            };
 
     function FinalWinWin(){
            alert("¡GANASTE XD!");
