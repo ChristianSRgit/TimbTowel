701798d51bb4be105036b554bdff88474355d4d7
christian Wirenet <christian.s.ramundo@gmail.com>
2023-01-05 16:14:43 -0300

    styles media adn rload btn

--- JS logic/menu&logs.js
+++ JS logic/menu&logs.js
@@ -3,7 +3,5 @@
      
      function refillPools(){
           userStashObj.$ = 10;
-          UniversalPoolObj.$ = 999;
           userStash.innerHTML = `$${userStashObj.$}`;
-          universalPool.innerHTML = `$${UniversalPoolObj.$}`
      }
